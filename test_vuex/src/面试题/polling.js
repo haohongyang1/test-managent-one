9f037c4478551a87fa8864270f4faedd28be8599
hhy <haohongyang@zuoyebang.com>
2020-04-24 23:00:40 +0800

    轮询函数更新

---
+++ test_vuex/src/面试题/polling.js
@@ -0,0 +1,41 @@
+// 实现支付结果轮询；每3s轮询一次，执行3次，终止轮询结果
+function pollingTest(fn, seconds, times, forwardVal) {
+  return function (...args) {
+    return new Promise((resolve) => {
+      let _this = this,
+        timerQueue = [];
+      for (let i = 1; i <= times; i++) {
+        timerQueue[i] = setTimeout(function () {
+          let res;
+          fn.apply(_this, [...args]).then((res) => {
+            res = res;
+            console.log(res);
+            if (res === forwardVal) {
+              for (let j = i + 1; j < timerQueue.length; j++) {
+                clearTimeout(timerQueue[j]);
+              }
+              resolve("成功拿到");
+            }
+            if (i === times) resolve("超时未拿到");
+          });
+        }, seconds * i);
+      }
+    });
+  };
+}
+
+function test(n) {
+  return new Promise((resolve) => {
+    setTimeout(function () {
+      resolve(n);
+    }, 2000);
+  });
+}
+pollingTest(
+  test,
+  3000,
+  4,
+  0
+)(1).then((res) => {
+  console.log(res);
+});

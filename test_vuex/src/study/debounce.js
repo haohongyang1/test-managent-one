ef4f49298bd8e62eab44de69e187fba2f8a633d2
郝虹阳 <haohongyang@haohongyangdeMacBook-Pro.local>
2020-11-23 10:29:50 +0800

    update

--- test_vuex/src/study/debounce.js
+++ test_vuex/src/study/debounce.js
@@ -1,22 +1,20 @@
 // 防抖应用 dou--debounce - 联想输入  高频事件后，等待设定时间后触发
 // 节流应用 liu--throttle -  鼠标移动事件    高频事件触发，n秒内执行一次
+
 const debounce = (fn, wait, immediate) => {
   let timeout;
   return function () {
-    const context = this;
-    const args = arguments;
-
     if (timeout) clearTimeout(timeout);
     if (immediate) {
       let run = !timeout;
       timeout = setTimeout(() => {
         timeout = null;
       }, wait);
-      if (run) fn.apply(context, args);
+      if (run) fn.apply(this, arguments);
     } else {
-      timeout = setTiemout(function () {
-        fn.apply(context, args);
-      }, await);
+      timeout = setTimeout(function () {
+        fn.apply(this, arguments);
+      }, wait);
     }
   };
 };
@@ -28,19 +26,17 @@ const trottle = (fn, wait, immediate) => {
     var timeout;
   }
   return function () {
-    const context = this;
-    const args = arguments;
     if (immediate) {
       let now = Date.now();
       if (now - pre >= wait) {
-        fn.apply(context, args);
+        fn.apply(this, arguments);
         pre = now;
       }
     } else {
       if (!timeout) {
         timeout = setTiemout(function () {
           timeout = null;
-          fn.apply(context, args);
+          fn.apply(this, arguments);
         }, wait);
       }
     }
